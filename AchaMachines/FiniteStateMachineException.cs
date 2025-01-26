333d4dcd17c86e0613aed82390cd78517f4d1a90
River Champeimont <river@almacha.org>
2025-01-25 20:32:55 -0500

    name change

--- AchaMachines/FiniteStateMachineException.cs
+++ AchaMachines/FiniteStateMachineException.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 Raphael Champeimont
+// Copyright (c) 2009 River Champeimont
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
